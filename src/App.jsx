67dff108bf6c342a891d2caade3e0772aa66e4c9
nuzulatha <222313305@stis.ac.id>
2025-07-14 09:29:10 +0700

    Initial Commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,79 +1,52 @@
-import React from 'react';
-import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
-import { AuthProvider, useAuth } from './context/AuthContext';
+// src/App.jsx 
+import React from "react"; 
+import { 
+  Routes, 
+  Route, 
+  Navigate, 
+} from "react-router-dom"; 
+import Navbar from "./components/Navbar"; 
+import PublicationListPage from "./components/PublicationListPage"; 
+import AddPublicationPage from "./components/AddPublicationPage"; 
+import Footer from "./components/Footer"; 
+import LoginPage from "./components/LoginPage"; 
+import EditPublicationPage from "./components/EditPublicationPage"; 
+import ProtectedRoute from "./components/ProtectedRoute";
 
-// Mengimpor semua komponen dari lokasi yang benar (folder components)
-import Navbar from "./components/Navbar";
-import Footer from "./components/Footer";
-import LoginPage from './components/LoginPage';
-import PublicationListPage from './components/PublicationListPage';
-import AddPublicationPage from './components/AddPublicationPage';
-import EditPublicationPage from './components/EditPublicationPage';
-import ProtectedRoute from './components/ProtectedRoute'; // Kita akan menggunakan komponen yang diimpor ini
+export default function App() { 
+  return ( 
+    <div className="bg-gray-100 min-h-screen font-sans"> 
+      <Navbar /> 
+      <main className="p-4 sm:p-6 lg:p-8"> 
+        <Routes> 
+          {/* Public Routes */}
+          <Route path="/login" element={<LoginPage />} />
+          <Route path="/publications" element={<PublicationListPage />} />
 
-// DEFINISI DUPLIKAT DIHAPUS DARI SINI
+          {/* Protected Routes */}
+          <Route
+            path="/publications/add"
+            element={
+              <ProtectedRoute>
+                <AddPublicationPage />
+              </ProtectedRoute>
+            }
+          />
+          <Route
+            path="/publications/edit/:id"
+            element={
+              <ProtectedRoute>
+                <EditPublicationPage />
+              </ProtectedRoute>
+            }
+          />
 
-/**
- * Komponen utama aplikasi yang mengatur semua rute.
- */
-function App() {
-    return (
-        // BrowserRouter harus membungkus semuanya
-        <BrowserRouter>
-            {/* AuthProvider harus membungkus semua yang butuh data login */}
-            <AuthProvider>
-                <div className="bg-gray-100 min-h-screen font-sans flex flex-col">
-                    <Navbar />
-                    <main className="flex-grow p-4 sm:p-6 lg:p-8">
-                        <Routes>
-                            {/* Rute Publik: /login bisa diakses siapa saja */}
-                            <Route path="/login" element={<LoginPage />} />
-
-                            {/* Rute yang Dilindungi: Dibungkus dengan <ProtectedRoute> */}
-                            <Route 
-                                path="/publications" 
-                                element={
-                                    <ProtectedRoute>
-                                        <PublicationListPage />
-                                    </ProtectedRoute>
-                                } 
-                            />
-                            <Route 
-                                path="/publications/add" 
-                                element={
-                                    <ProtectedRoute>
-                                        <AddPublicationPage />
-                                    </ProtectedRoute>
-                                } 
-                            />
-                            <Route 
-                                path="/publications/edit/:id" 
-                                element={
-                                    <ProtectedRoute>
-                                        <EditPublicationPage />
-                                    </ProtectedRoute>
-                                } 
-                            />
-
-                            {/* Rute Default: Jika pengguna membuka alamat utama ('/'),
-                                arahkan ke daftar publikasi. */}
-                            <Route 
-                                path="/" 
-                                element={
-                                    <ProtectedRoute>
-                                        <Navigate to="/publications" replace />
-                                    </ProtectedRoute>
-                                } 
-                            />
-                             {/* Rute Catch-all untuk halaman yang tidak ditemukan */}
-                            <Route path="*" element={<Navigate to="/publications" replace />} />
-                        </Routes>
-                    </main>
-                    <Footer />
-                </div>
-            </AuthProvider>
-        </BrowserRouter>
-    );
-}
-
-export default App;
+          {/* Redirect Routes */}
+          <Route path="/" element={<Navigate to="/publications" replace />} />
+          <Route path="*" element={<Navigate to="/publications" replace />} />
+        </Routes> 
+      </main> 
+      <Footer /> 
+    </div> 
+  ); 
+} 
